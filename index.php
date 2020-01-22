a8a25a7fc4187571ffe77d6dd1599c70115c5e7b
Eileen S <eileen.j.sul@gmail.com>
2020-01-22 17:48:46 -0600

    updated title

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Crypto Prediction App in PHP</title>
+    <title>Bitcoin Price App in PHP</title>
     <link rel="stylesheet" href="input.css">
     <link rel="shortcut icon" href="/favicon-16x16.png">
 
